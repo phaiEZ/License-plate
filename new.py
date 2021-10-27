7d861804b3b9aca75f53a9ac7e40ad8458a9da20
phai <51281702+phaiEZ@users.noreply.github.com>
2021-10-28 02:22:54 +0700

    Add files via upload

--- new.py
+++ new.py
@@ -2,6 +2,7 @@ import cv2
 import numpy
 import matplotlib.pyplot as ply
 import pytesseract  
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 from PIL import Image, ImageGrab
 
 def convertImage(image):
@@ -10,17 +11,17 @@ def convertImage(image):
     canny = cv2.Canny(gray,100,200)
     return canny
 while True :
-    cap = cv2.VideoCapture(0)
-    while (True) :
-        ret,frame = cap.read()
-        cv2.imshow("frame",frame)
-        if (cv2.waitKey(1) & 0xFF == ord("a")) :
-            cv2.imwrite('123.jpg',frame)
-            break
-        if (cv2.waitKey(1) & 0xFF == ord("o")) :
-            break
+    # cap = cv2.imread("w644.jpg")
+    # while (True) :
+    #     ret,frame = cap.read()
+    #     cv2.imshow("frame",frame)
+    #     if (cv2.waitKey(1) & 0xFF == ord("a")) :
+    #         cv2.imwrite('123.jpg',frame)
+    #         break
+    #     if (cv2.waitKey(1) & 0xFF == ord("o")) :
+    #         break
 
-    img = cv2.imread("123.jpg")
+    img = cv2.imread("w644.jpg")
     processed_img = convertImage(img)
     contour_img = processed_img.copy()
     original_img = img.copy()
@@ -39,9 +40,13 @@ while True :
 
     cv2.imshow("license Detected :",license_img)
     cv2.imshow("test",img)
-    cv2.imwrite('F:\code\chatbot/456.jpg',license_img)
+
+    cv2.imwrite('C:/Users/phai2/Downloads/License-plate-main/456.jpg',license_img)
     if __name__ == "__main__":
         img = Image.open("456.jpg")
-        x = pytesseract.image_to_string(img,lang ='tha')
+        x = pytesseract.image_to_string(img,lang ='tha+eng')
         print(x)
+
+        if("2019" in x and "รถ" in x and "กรุงเทพมหานคร" in x):
+            print("จริง")
     cv2.waitKey(0)
